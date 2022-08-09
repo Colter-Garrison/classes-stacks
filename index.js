493de2cfc6371185cdc393ca4798118d5950133c
Allison Ause <allison.ause@gmail.com>
2022-08-09 10:46:51 -0700

    working on stack

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-class Stack {}
+class Stack {
+  list = []
+
+  constructor(initialList) {
+    this.list = initialList
+  }
+
+  push(item) {
+    this.list = [...this.list, item]
+    console.log('this.list:', this.list)
+    return this.list
+  }
+
+  pop() {
+    this.list = this.list.length - 1
+    return this.list
+  }
+
+  peek() {}
+}
 
 class Queue {}
 
-module.exports = { Stack, Queue };
+const newStack = new Stack(['duck', 'llama'])
+// newStack.push('fox')
+newStack.pop()
+console.log(newStack)
+
+module.exports = { Stack, Queue }
